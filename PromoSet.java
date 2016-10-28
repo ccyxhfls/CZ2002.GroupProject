0f19209fb966bd3cb9d148efcab7a3a4283f5013
ccyxhfls <ccyxhfls@gmail.com>
2016-10-28 14:12:25 +0800

    Add files via upload

--- PromoSet.java
+++ PromoSet.java
@@ -1,58 +1,57 @@
 package restaurantReservationApp;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-import restaurantReservationApp.Food.Category;
+public class PromoSet extends MenuItem {
+	ArrayList<Food> foodList;
 
-public class PromoSet {
-	private ArrayList<Food> foodList;
-	private String name;
-	private int ID;
-	private double totalPrice;
-	private double promoPrice;
+	public PromoSet(int ID){
+		this.ID = ID;
+		foodList = new ArrayList<Food>();
+	}
 	
 	public PromoSet (String[] metaData){
 		ID = Integer.parseInt(metaData[0]);
-		totalPrice = Double.parseDouble(metaData[0]);
-		promoPrice = Double.parseDouble(metaData[0]);
+		price = Double.parseDouble(metaData[1]);
+		discountPrice = Double.parseDouble(metaData[2]);
 	}
 	
-	public PromoSet (String name, double totalPrice, double PromoPrice){
-		setName(name);
-		setTotalPrice(totalPrice);
-		setPromoPrice(promoPrice);
-	}
-	
-	public int getID(){
-		return ID;
+	public PromoSet(int ID, String name, ArrayList<Food> foodList, double price, String description) {
+		this.ID = ID;
+		this.name = name;
+		this.foodList = foodList;
+		System.out.println(foodList.size());
+		
+		this.discountPrice = price;
+		this.description = description;
+		
 	}
 
-	
-	public void setID(int ID){
-		this.ID= ID;
-	}
-	
-	public void setName(String name){
-		this.name = name;
-	}
-	
-	
-	public void setTotalPrice(double totalPrice){
-		this.totalPrice = totalPrice;
-	}
-	
-	public void setPromoPrice(double promoPrice){
-		this.promoPrice = promoPrice;
-	}
-	
 	public Food addFood(Food food){
 		foodList.add(food);
+		price += food.getPrice();
 		return food;
 	}
 	
 	public Food removeFood(Food food){
 		foodList.remove(food);
+		price -= food.getPrice();
 		return food;
 	}
 
+	public void print() {
+		System.out.println("Set ID:" + ID);
+		
+		Iterator<Food> itr = foodList.iterator();
+		while(itr.hasNext()){
+			Food food = itr.next();
+			System.out.println(food.toString());
+		}
+		System.out.println("total:" + price);
+		System.out.println("discount price:" + discountPrice);
+
+	}
+	
+	
 }
